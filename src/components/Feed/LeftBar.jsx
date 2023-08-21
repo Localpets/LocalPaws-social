ad251a445937d8271db6e3e1e832f763e2f987df
Diego Garcia <dagadev@proton.me>
2023-08-20 21:04:24 -0500

    Merge from auth

--- src/components/Feed/LeftBar.jsx
+++ src/components/Feed/LeftBar.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/Icons/logo.png'
+
 import { Link } from '@tanstack/router'
 const LeftBar = () => {
   return (
