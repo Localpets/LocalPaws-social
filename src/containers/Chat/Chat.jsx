377c59c47ebf7279ada97b9e6b62b529ae1b4efb
Ricardoarsv <ricardoarsv.2004@gmail.com>
2023-08-28 13:28:57 -0500

    Codigo comentado y parte 1 de manejo de visibilidad

--- src/containers/Chat/Chat.jsx
+++ src/containers/Chat/Chat.jsx
@@ -1,3 +1,5 @@
+// Importación de librerías y componentes necesarios
+import { useEffect, useState, useRef } from 'react'
 import {
   BsFillChatDotsFill,
   BsFillPeopleFill,
@@ -8,17 +10,33 @@ import {
 import { Link } from '@tanstack/router'
 import useChatStore from '../../context/ChatStore'
 import { makeRequest } from '../../library/axios'
-import { useEffect, useState, useRef } from 'react'
+import './Chat.css'
 
+// Componente principal Chat
 const Chat = () => {
-  const { sideContactsStyle, toggleSideContactsStyle, toggleHamburguerStyle } = useChatStore()
-  const [allchats, setAllchats] = useState([])
-  const [localuser, setLocaluser] = useState([])
-  const [users, setUsers] = useState([])
-  const [currentchat, setCurrentchat] = useState([])
-  const [message, setMessage] = useState('')
-  const chatContainerRef = useRef(null)
+  // Extracción de funciones y estados del contexto
+  const {
+    sideContactsStyle,
+    toggleSideContactsStyle,
+    toggleHamburguerStyle
+  } = useChatStore()
 
+  // Estados para manejar los datos de la conversación y los mensajes
+  const [allchats, setAllchats] = useState([])
+  // Estado para almacenar los datos del usuario logueado
+  const [localuser, setLocaluser] = useState([])
+  // Estado para controlar los usuarios de la api
+  const [users, setUsers] = useState([])
+  // Estado para controlar el chat actualmente abierto
+  const [currentchat, setCurrentchat] = useState([])
+  // Estado para controlar el formulario de envio de mensajes
+  const [message, setMessage] = useState('')
+  // Estado para controlar la posicion de la barra vertical
+  const chatContainerRef = useRef(null)
+  // Estado para controlar el mensaje expandido por su id
+  const [expandedMessageId, setExpandedMessageId] = useState(null)
+
+  // useEffect para obtener y configurar los datos del usuario local
   useEffect(() => {
     if (localStorage.getItem('user')) {
       const user = JSON.parse(localStorage.getItem('user'))
@@ -26,28 +44,34 @@ const Chat = () => {
     }
   }, [setLocaluser])
 
+  // useEffect para ajustar el scrollTop y mantener la barra al final del chat
   useEffect(() => {
-    // Ajusta el scrollTop para mantener la barra al final
     if (chatContainerRef.current) {
       chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
     }
   }, [currentchat.conversation])
 
+  // useEffect para obtener los mensajes de la conversación y usuarios
   useEffect(() => {
     async function fetchAllChats () {
       try {
         if (localuser.userId) {
-          const response = await makeRequest.get(`message/find/all/${localuser.userId}`)
+          // Obtener mensajes de la API
+          const response = await makeRequest.get(
+            `message/find/all/${localuser.userId}`
+          )
           const chats = response.data.messages
-          /* console.log('la lista de chats:', chats) */
           setAllchats(chats)
 
           // Crear un objeto para agrupar los mensajes por conversación
           const conversationMap = {}
 
           // Iterar a través de los mensajes y agruparlos en el objeto
-          chats.forEach(chat => {
-            const conversationKey = `${Math.min(chat.sender_id, chat.receiver_id)}-${Math.max(chat.sender_id, chat.receiver_id)}`
+          chats.forEach((chat) => {
+            const conversationKey = `${Math.min(
+              chat.sender_id,
+              chat.receiver_id
+            )}-${Math.max(chat.sender_id, chat.receiver_id)}`
 
             if (!conversationMap[conversationKey]) {
               conversationMap[conversationKey] = []
@@ -58,12 +82,11 @@ const Chat = () => {
 
           // Convertir el objeto en una lista de conversaciones
           const conversationList = Object.values(conversationMap)
-
           setAllchats(conversationList)
 
+          // Obtener información de usuarios de la API
           const usersResponse = await makeRequest.get('user/find/all')
           const userList = usersResponse.data.data
-          /* console.log('la lista de usuarios:', userList) */
           setUsers(userList)
         }
       } catch (error) {
@@ -74,10 +97,10 @@ const Chat = () => {
     fetchAllChats()
   }, [localuser.userId, allchats])
 
+  // Función para cargar los mensajes de una conversación
   const loadConversationMessages = (conversation) => {
     setCurrentchat(conversation)
   }
-
   // Maneja el envío del formulario
   const handleSubmit = async (event) => {
     event.preventDefault()
@@ -106,11 +129,93 @@ const Chat = () => {
     }
   }
 
+  // Función para formatear el texto de los mensajes, dividiéndolo en líneas y limitando su longitud
+  const formatMessageText = (text, messageId) => {
+    const maxWordsPerLine = 8
+    const maxVisibleLines = 3
+
+    // Dividir el texto en palabras y preparar variables para el formateo
+    const words = text.split(' ')
+    const lines = []
+    let currentLine = ''
+    let lineWordCount = 0
+
+    // Iterar a través de las palabras para formatear el texto en líneas
+    for (const word of words) {
+      if (lineWordCount + 1 + word.split('\n').length - 1 <= maxWordsPerLine) {
+        currentLine += (currentLine.length > 0 ? ' ' : '') + word
+        lineWordCount += word.split('\n').length
+      } else {
+        lines.push(currentLine)
+        currentLine = word
+        lineWordCount = word.split('\n').length
+      }
+    }
+
+    if (currentLine.length > 0) {
+      lines.push(currentLine)
+    }
+
+    // Dividir las líneas en líneas visibles y líneas restantes
+    const visibleLines = lines.slice(0, maxVisibleLines)
+    const remainingLines = lines.slice(maxVisibleLines)
+
+    // Verificar si el mensaje está expandido
+    const isExpanded = expandedMessageId === messageId
+
+    return (
+      <>
+        {visibleLines.map((line, index) => (
+          <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
+        ))}
+        {remainingLines.length > 0 && (
+          <>
+            {isExpanded
+              ? (
+                <>
+                  {remainingLines.map((line, index) => (
+                    <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
+                  ))}
+                  <button
+                    className='btn-ghost'
+                    onClick={() => handleReadMore(messageId)}
+                  >
+                    Leer menos
+                  </button>
+                </>
+                )
+              : (
+                <button
+                  className='btn-ghost'
+                  onClick={() => handleReadMore(messageId)}
+                >
+                  Leer más
+                </button>
+                )}
+          </>
+        )}
+      </>
+    )
+  }
+
+  // Función para manejar la expansión o contracción de los mensajes
+  const handleReadMore = (messageId) => {
+    if (expandedMessageId === messageId) {
+      setExpandedMessageId(null)
+    } else {
+      setExpandedMessageId(messageId)
+    }
+  }
+
+  // Sección principal del componente Chat
   return (
     <section className='h-[100vh] flex w-full container'>
+      {/* Panel lateral izquierdo en pantallas medianas y grandes */}
       <section className='hidden w-40 h-full border-r-2 bg-[#1B263B] md:flex flex-col justify-between items-center gap-18 p-4 py-8'>
         <h1 className='font-bold text-white text-xl'>PawsPlorer Messenger</h1>
         <ul className='flex flex-col items-center justify-center gap-8'>
+
+          {/* Botones de íconos */}
           <li>
             <Link className='btn btn-ghost'>
               <BsFillChatDotsFill className='text-white text-2xl' />
@@ -127,6 +232,8 @@ const Chat = () => {
             </Link>
           </li>
         </ul>
+
+        {/* Botón con imagen de perfil */}
         <button>
           <img
             className='w-10 h-10 rounded-full'
@@ -135,7 +242,9 @@ const Chat = () => {
         </button>
       </section>
 
+      {/* Panel lateral izquierdo en pantallas pequeñas */}
       <section className='hidden md:w-60 lg:w-80 h-full bg-white md:flex flex-col justify-start items-center gap-18 p-4 py-8 pt-6'>
+        {/* Título y botones */}
         <ul className='w-full flex flex-wrap items-center justify-around pb-2'>
           <li>
             <h2 className='font-bold text-slate-800 text-xl'>Conversations</h2>
@@ -147,12 +256,14 @@ const Chat = () => {
           </li>
         </ul>
 
+        {/* Búsqueda de conversaciones */}
         <input
           type='text'
           placeholder='Buscar conversación'
           className='input bg-gray-100 text-black rounded-md mt-4 w-full max-w-2xl placeholder:font-semibold'
         />
 
+        {/* Lista de conversaciones */}
         <ul className='flex flex-col w-full items-center justify-center gap-4 pt-8'>
           {allchats.map(conversation => {
             // Obtén la información del último mensaje en la conversación
@@ -172,8 +283,8 @@ const Chat = () => {
             // Limitar el nombre de usuario a 30 caracteres
             const limitedUsername = otherUser ? otherUser.username.substring(0, 30) : ''
 
-            // Limitar el mensaje a 30 caracteres
-            const limitedMessage = lastMessage.text.length > 30 ? lastMessage.text.substring(0, 30) + '...' : lastMessage.text
+            // Limitar el mensaje a 30 caracteres y añadir puntos suspensivos
+            const limitedMessage = lastMessage.text.length > 10 ? lastMessage.text.substring(0, 30) + '...' : lastMessage.text
 
             return (
               <li className='w-full' key={lastMessage.id}>
@@ -205,6 +316,7 @@ const Chat = () => {
       {/* mobile menu */}
 
       <section className={`z-10 md:hidden w-full   h-full bg-white flex flex-col justify-start items-center gap-18 p-4 ${sideContactsStyle} transition-transform`}>
+        {/* Título y botones en menú móvil */}
         <ul className='w-full flex flex-wrap items-center justify-start pb-2 gap-4'>
           <li>
             <h2 className='font-bold text-slate-800 text-xl'>Conversations</h2>
@@ -216,15 +328,18 @@ const Chat = () => {
           </li>
         </ul>
 
+        {/* Búsqueda de conversaciones en menú móvil */}
         <section className='w-full h-[80%]'>
           <input
             type='text'
             placeholder='Buscar conversación'
             className='input bg-gray-100 text-black rounded-md mt-4 w-full max-w-2xl placeholder:font-semibold'
           />
+
+          {/* Lista de conversaciones en menú móvil */}
           <ul className='flex flex-col w-full items-center justify-center gap-4 pt-8'>
             {allchats.map(conversation => {
-              // Obtén la información del último mensaje en la conversación
+            // Obtén la información del último mensaje en la conversación
               const lastMessage = conversation[0]
 
               // Determinar el ID del otro usuario en la conversación
@@ -241,8 +356,8 @@ const Chat = () => {
               // Limitar el nombre de usuario a 30 caracteres
               const limitedUsername = otherUser ? otherUser.username.substring(0, 30) : ''
 
-              // Limitar el mensaje a 30 caracteres
-              const limitedMessage = lastMessage.text.length > 30 ? lastMessage.text.substring(0, 30) + '...' : lastMessage.text
+              // Limitar el mensaje a 30 caracteres y añadir puntos suspensivos
+              const limitedMessage = lastMessage.text.length > 10 ? lastMessage.text.substring(0, 30) + '...' : lastMessage.text
 
               return (
                 <li className='w-full' key={lastMessage.id}>
@@ -271,9 +386,12 @@ const Chat = () => {
           </ul>
         </section>
 
+        {/* Sección final con botones en menú móvil */}
         <section className=' w-full bg-[#1B263B] flex flex-col justify-between items-center justify-self-end gap-4 p-4 rounded-lg'>
           <h1 className='font-bold text-white text-xl'>PawsPlorer Messenger</h1>
           <ul className='flex flex-row items-center justify-center'>
+
+            {/* Botones de íconos en menú móvil */}
             <li>
               <Link className='btn btn-ghost'>
                 <BsFillChatDotsFill className='text-white text-2xl' />
@@ -300,7 +418,11 @@ const Chat = () => {
           </ul>
         </section>
       </section>
+
+      {/* Sección de conversación principal */}
       <section className='border-2 flex flex-col justify-start flex-1 h-full w-full'>
+
+        {/* Encabezado de la conversación */}
         <section className='flex w-full items-center justify-between px-2 bg-white md:rounded-tr-lg'>
           <div className='flex'>
             <div className='flex items-center gap-2 py-4 md:gap-4 md:pt-4 md:pl-4 md:pb-4'>
@@ -314,8 +436,10 @@ const Chat = () => {
               </h2>
             </div>
           </div>
+
+          {/* Control de menú lateral en modo móvil */}
           <label className='z-20 btn btn-sm btn-circle swap swap-rotate mr-2 md:hidden'>
-            {/* this hidden checkbox controls the state */}
+            {/* Checkbox oculto que controla el estado */}
             <input
               type='checkbox' className='hidden' onClick={() => {
                 toggleSideContactsStyle()
@@ -347,9 +471,14 @@ const Chat = () => {
           </label>
         </section>
 
+        {/* Contenido de la conversación */}
         <section className='h-full overflow-auto'>
           <div className='flex flex-col w-full h-full justify-end bg-[url("https://wallpapercave.com/wp/wp4410779.png")] bg-cover rounded-br-lg'>
+
+            {/* Mensajes de la conversación */}
             <section className='p-4 pb-6 overflow-auto' ref={chatContainerRef}>
+
+              {/* Mapeo de mensajes */}
               {Array.isArray(currentchat.conversation)
                 ? (
                     currentchat.conversation.slice().reverse().map(message => (
@@ -357,22 +486,30 @@ const Chat = () => {
                         className={`chat ${message.sender_id === localuser.userId ? 'chat-end' : 'chat-start'}`}
                         key={message.id}
                       >
-                        <div className='chat-bubble bg-[#1B263B] text-white'>{message.text}</div>
+                        <div className='chat-bubble bg-[#1B263B] text-white flex items-start text-left gap-2'>
+                          <div className='message-content'>
+                            <div className={`message-text ${expandedMessageId === message.id ? 'expanded' : ''}`}>
+                              {formatMessageText(message.text, message.id)}
+                            </div>
+                          </div>
+                          <button> ᐁ </button>
+                        </div>
                       </div>
                     ))
-
                   )
                 : (
-                  <p className='text-black font-bold'>Inicia una conversacion</p>
+                  <p className='text-black font-bold'>Inicia una conversación</p>
                   )}
             </section>
 
+            {/* Formulario de envío de mensaje */}
             <form className='p-4 pb-4 flex gap-4 items-center'>
               <input
                 type='text'
                 placeholder='Mensaje'
                 className='input bg-white text-black rounded-md w-full max-w-2xl placeholder:font-semibold'
                 value={message}
+                maxLength={500}
                 onChange={(e) => setMessage(e.target.value)}
               />
               <button
